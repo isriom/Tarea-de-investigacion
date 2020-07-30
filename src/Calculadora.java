6039b1c7100e18bb11c98110ca01530219561ebe
isriom <62965230+isriom@users.noreply.github.com>
2020-07-30 17:03:06 -0600

    Added asin, acos, atan, and mathemathical written of function

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -5,9 +5,9 @@ import static java.lang.Math.PI;
 public class Calculadora {
     public static void main(String[] args){
         System.out.println("Bienvenido a la calculadora de funciones trigonometricas");
-        System.out.println("Sí desea calcular: seno, coseno, y tangente ingresa 1");
-        System.out.println("Sí desea calcular: arcoseno, arcocoseno, y arcotangente ingresa 2");
-        System.out.println("Sí desea calcular: senh, cosh, y tanh ingresa 3");
+        System.out.println("Sí desea calcular: seno, coseno, y tangente ingresa: 1");
+        System.out.println("Sí desea calcular: arcoseno, arcocoseno, y arcotangente ingresa: 2");
+        System.out.println("Sí desea calcular: senh, cosh, y tanh ingresa: 3");
         double scan= Calculadora.numero();
         int scanned=(int)scan;
         boolean standar= scanned==1;
@@ -21,7 +21,7 @@ public class Calculadora {
             Calculadora.arc();
         }
         else if (complementarias){
-            Calculadora.standar();
+            Calculadora.complementarias();
         }else{
             System.out.println("Por favor ingresa un numero valido");
             System.out.println("reiniciando");
@@ -31,25 +31,37 @@ public class Calculadora {
 
 
     }
-    public static void standar(){
+    public static void standar() {
         System.out.println("Dame el valor de angulo y te dare seno, coseno y tangente del angulo:");
-        System.out.println("(recuerda usar , para los decimales)");
-        double scan=Calculadora.numero();
-        scan=scan * PI / 180;
-        System.out.println("El angulo es: "+scan);
-        System.out.println("El valor de seno para este ángulo es :"+Math.sin(scan));
-        System.out.println("El valor de coseno para este ángulo es :"+Math.cos(scan));
-        System.out.println("El valor de tangente para este ángulo es :"+Math.tan(scan));
-
+        System.out.println("(recuerda usar , para los decimales e introducirlo en grados)");
+        double scan = Calculadora.numero();
+        System.out.println("El angulo es: " + scan);
+        scan = scan * PI / 180;
+        System.out.println("El valor de seno para este ángulo es :" + Math.sin(scan));
+        System.out.println("El valor de coseno para este ángulo es :" + Math.cos(scan));
+        System.out.println("El valor de tangente para este ángulo es :" + Math.tan(scan));
     }
+
     public static void arc(){
-        System.out.println("Dame el valor de angulo y te dare arcoseno, arcocoseno y arcotangente del angulo:");
+        System.out.println("Dame un valor y te dare arcoseno, arcocoseno y arcotangente del valor:");
         System.out.println("(recuerda usar , para los decimales)");
         double scan=Calculadora.numero();
+        System.out.println("El valor es: "+scan);
+        System.out.println("El valor en grados de arcoseno es :"+(Math.asin(scan))* 180/PI);
+        System.out.println("El valor en grados de arcocoseno  es :"+(Math.acos(scan))* 180/PI);
+        System.out.println("El valor en grados de arcotangente es :"+(Math.atan(scan))* 180/PI);
+    }
+
+    public static void complementarias(){
+        System.out.println("Dame el valor de angulo y te dare senh, cosh y tanh del angulo:");
+        System.out.println("(recuerda usar , para los decimales e introducirlo en grados)");
+        double scan=Calculadora.numero();
         System.out.println("El angulo es: "+scan);
-        System.out.println("El valor de arcoseno para este ángulo es :"+Math.asin(scan));
-        System.out.println("El valor de arcocoseno para este ángulo es :"+Math.acos(scan));
-        System.out.println("El valor de arcotangente para este ángulo es :"+Math.atan(scan));
+        scan=scan * PI / 180;
+        System.out.println("El valor de senh para este ángulo es :"+Math.sinh(scan));
+        System.out.println("El valor de cosh para este ángulo es :"+Math.cosh(scan));
+        System.out.println("El valor de tanh para este ángulo es :"+Math.tanh(scan));
+
     }
     public static double numero(){
         Scanner scanner=new Scanner(System.in);
