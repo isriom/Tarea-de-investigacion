83816f10df6741c44411296dca2b6af4ae203a70
Brandon Reyes Alvarado <67602859+Brandon0309@users.noreply.github.com>
2020-08-08 18:44:49 -0600

    Typing corrections

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -33,34 +33,34 @@ public class Calculadora {
     }
     public static void standar() {
         System.out.println("Dame el valor de angulo y te dare seno, coseno y tangente del angulo:");
-        System.out.println("(recuerda usar , para los decimales e introducirlo en grados)");
+        System.out.println("(Recuerda usar , para los decimales e introducirlo en grados)");
         double scan = Calculadora.numero();
         System.out.println("El angulo es: " + scan);
         scan = scan * PI / 180;
-        System.out.println("El valor de seno para este ángulo es :" + Math.sin(scan));
-        System.out.println("El valor de coseno para este ángulo es :" + Math.cos(scan));
-        System.out.println("El valor de tangente para este ángulo es :" + Math.tan(scan));
+        System.out.println("El valor de Seno para este ángulo es :" + Math.sin(scan));
+        System.out.println("El valor de Coseno para este ángulo es :" + Math.cos(scan));
+        System.out.println("El valor de Tangente para este ángulo es :" + Math.tan(scan));
     }
 
     public static void arc(){
-        System.out.println("Dame un valor y te dare arcoseno, arcocoseno y arcotangente del valor:");
-        System.out.println("(recuerda usar , para los decimales)");
+        System.out.println("Dame un valor y te dare Arcoseno, Arcocoseno y Arcotangente del valor:");
+        System.out.println("(Recuerda usar , para los decimales)");
         double scan=Calculadora.numero();
         System.out.println("El valor es: "+scan);
-        System.out.println("El valor en grados de arcoseno es :"+(Math.asin(scan))* 180/PI);
-        System.out.println("El valor en grados de arcocoseno  es :"+(Math.acos(scan))* 180/PI);
-        System.out.println("El valor en grados de arcotangente es :"+(Math.atan(scan))* 180/PI);
+        System.out.println("El valor en grados de Arcoseno es :"+(Math.asin(scan))* 180/PI);
+        System.out.println("El valor en grados de Arcocoseno  es :"+(Math.acos(scan))* 180/PI);
+        System.out.println("El valor en grados de Arcotangente es :"+(Math.atan(scan))* 180/PI);
     }
 
     public static void complementarias(){
-        System.out.println("Dame el valor de angulo y te dare senh, cosh y tanh del angulo:");
-        System.out.println("(recuerda usar , para los decimales e introducirlo en grados)");
+        System.out.println("Dame el valor de angulo y te dare el Seno Hiperbólico, Coseno Hiperbólico y Tangente Hiperbólico del angulo:");
+        System.out.println("(Recuerda usar , para los decimales e introducirlo en grados)");
         double scan=Calculadora.numero();
         System.out.println("El angulo es: "+scan);
         scan=scan * PI / 180;
-        System.out.println("El valor de senh para este ángulo es :"+Math.sinh(scan));
-        System.out.println("El valor de cosh para este ángulo es :"+Math.cosh(scan));
-        System.out.println("El valor de tanh para este ángulo es :"+Math.tanh(scan));
+        System.out.println("El valor del Seno Hiperbólico para este ángulo es :"+Math.sinh(scan));
+        System.out.println("El valor de Coseno Hiperbólico para este ángulo es :"+Math.cosh(scan));
+        System.out.println("El valor de Tangente Hiperbólico para este ángulo es :"+Math.tanh(scan));
 
     }
     public static double numero(){
